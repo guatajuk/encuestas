df7f9af0e686d6459a067e72db68f5f70e5991b5
guatajuk <sp0t.col@gmail.com>
2014-11-06 23:17:33 -0500

    Se paginan las preguntas, por defecto se muestran 10 items

--- encuestas/app/controllers/questions_controller.rb
+++ encuestas/app/controllers/questions_controller.rb
@@ -3,7 +3,7 @@ class QuestionsController < ApplicationController
   before_action :set_question, only: [:show, :edit, :update, :destroy]
 
   def index
-    @questions = Question.all
+    @questions = Question.all.page params[:page]
     respond_with(@questions)
   end
 
--- encuestas/app/models/question.rb
+++ encuestas/app/models/question.rb
@@ -1,6 +1,8 @@
 class Question
   include Mongoid::Document
   include Mongoid::Timestamps
+
   field :item, type: String
   field :aspect, type: String
+
 end
