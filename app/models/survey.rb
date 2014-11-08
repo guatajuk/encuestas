f6bb6f0553752d78642d0d9b56082b2e5b4e843a
guatajuk <sp0t.col@gmail.com>
2014-11-08 12:14:51 -0500

    Se agregan restricciones a los modelos, se identa el 'application.html.erb'

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -1,8 +1,13 @@
 class Survey
   include Mongoid::Document
   include Mongoid::Timestamps
+  
   field :name, type: String
   field :deadline, type: Date
+
+	validates_uniqueness_of :name
+	validates_presence_of :name, :deadline
+
 
   has_many :questions
   belongs_to :survey_type
