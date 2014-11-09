8703f658e7143006703051cc0dde9b9b981af099
guatajuk <sp0t.col@gmail.com>
2014-11-09 00:33:01 -0500

    Se agrega hack para que los permisos funcionen en rails 4

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,11 @@ class ApplicationController < ActionController::Base
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to new_user_session_path, :alert => exception.message
   end
-  
+
+  before_filter do
+    resource = controller_name.singularize.to_sym
+    method = "#{resource}_params"
+    params[resource] &&= send(method) if respond_to?(method, true)
+  end
+
 end
