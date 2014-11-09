80d10a1b64d79bdf4787d2c0dca8b6306918fe5e
guatajuk <sp0t.col@gmail.com>
2014-11-09 00:01:48 -0500

    Se crean los roles para el usuario

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User
   include Mongoid::Document
+  rolify
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
