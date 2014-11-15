f7ba75d5b12372f5e7d6777f240e22c484d82a8b
guatajuk <sp0t.col@gmail.com>
2014-11-14 22:38:19 -0500

    Se agrega el control de 'devise' y 'cancancan' para el modelo 'course'

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,6 +2,8 @@ class Course
   include Mongoid::Document
   include Mongoid::Timestamps
 
+  resourcify
+  
   field :name, type: String
   field :course_id, type: String
   field :group, type: String
