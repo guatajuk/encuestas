e515d70a6f47265670a0443a07b93b4ecaab90b2
guatajuk <sp0t.col@gmail.com>
2014-11-19 10:35:14 -0500

    Se refactoriza la vista y el controlador para cargar usuarios

--- app/controllers/load_user_controller.rb
+++ app/controllers/load_user_controller.rb
@@ -1,13 +1,14 @@
 class LoadUserController < ApplicationController
+	respond_to :html, :xml, :json
 
 	def index
 	end
 
-	def load_soap
+	def load_data
 		id = params[:user_id]
 		10.times { puts "" }		
 		puts id
 		10.times { puts ""}
 	end
-
+	
 end
