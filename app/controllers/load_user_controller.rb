a8ea5cc666e3b2d7adb8392eb6ed18d0e05ec33c
guatajuk <sp0t.col@gmail.com>
2014-11-19 21:59:10 -0500

    Se modifica controlador y se valida que la entrada del id del usuario sea correcta para cargar datos del SOAP

--- app/controllers/load_user_controller.rb
+++ app/controllers/load_user_controller.rb
@@ -6,7 +6,13 @@ class LoadUserController < ApplicationController
 
 	def load_data
 		id = params[:user_id]
-		puts id
+		if id.to_i != 0
+			@user = User.create(name: "Ghoul_1", password:"123123123", email: "ghoul1@seu.com", id_number: id)
+			@user.add_role "Student"
+		else
+			@user = nil
+		end
+		@user
 	end
 	
 end
