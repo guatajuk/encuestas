43c6ebb4ee969c4e4930dcbeb61f49f970515e27
guatajuk <sp0t.col@gmail.com>
2014-12-03 01:33:33 -0500

    Se modifican permisos del administrador para que no pueda ver las encuestas

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,7 +4,7 @@ class Ability
   def initialize(user)
     user ||= User.new # guest user (not logged in)
     if user.has_role? "Admin"
-        can :manage, :all
+      can :manage, [Survey, SurveyType, Aspect, Question, Course, User]
     elsif user.has_role? "Student"
     	can [:update, :read], AnswerForm
     elsif user.has_role? "Teacher"
